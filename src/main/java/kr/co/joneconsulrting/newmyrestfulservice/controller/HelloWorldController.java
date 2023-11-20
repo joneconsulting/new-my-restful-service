5c2d9634a4f5acebc08cc570c4902983263c2f93
edowon0623@gmail.com <Dwlee$09>
2023-11-20 21:36:07 +0900

    Commit for Section 4

--- src/main/java/kr/co/joneconsulrting/newmyrestfulservice/controller/HelloWorldController.java
+++ src/main/java/kr/co/joneconsulrting/newmyrestfulservice/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package kr.co.joneconsulrting.newmyrestfulservice.controller;
 
 import kr.co.joneconsulrting.newmyrestfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -21,5 +31,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s ", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name="Accept-Language", required=false)  Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
